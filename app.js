2f50a6b6a7b42506755309dbadd43738531e0b48
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-08-18 19:18:03 +0300

    63- use express.listen() and remove http module

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const { text, buffer } = require("stream/consumers");
 
 const { error } = require("console");
-const http = require("http");
 
 const express = require("express");
 const app = express();
@@ -15,6 +14,5 @@ app.use((req, res, next) => {
   console.log("hi it's middleware 2");
   res.send("<h1>hello from Express!</h1>");
 });
-const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
