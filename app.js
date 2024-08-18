e8839f6f7bb01ceed3a523590705832e4666a144
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-08-18 19:08:15 +0300

    61- add middleware

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const http = require("http");
 
 const express = require("express");
 const app = express();
+
+app.use((req, res, next) => {
+  console.log("hi it's middleware 1");
+  next(); // Allows the request to continue to the next middleware in line.
+});
+
+app.use((req, res, next) => {
+  console.log("hi it's middleware 2");
+});
 const server = http.createServer(app);
 
 server.listen(3000);
