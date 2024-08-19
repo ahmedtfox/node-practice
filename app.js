633b3ec2d86f865116d8ff469a29759dbd8c250e
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-08-19 14:59:54 +0300

    66 limiting middleware execution

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.use("/add-product", (req, res, next) => {
 </form>`);
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
   console.log(req.body);
-  res.send("<h1>hello from Express!</h1>");
+  res.send("<h1>this is product!</h1>");
 });
 
 app.use("/", (req, res, next) => {
