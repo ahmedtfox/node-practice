c5459e425fd57dee2a4174c3d9100c561bf1297b
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-08-21 19:08:26 +0300

    Converting HTML Files to Pug

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const bodyParser = require("body-parser");
 app.set("view engine", "pug");
 app.set("views", "views");
 
-
 app.use(bodyParser.urlencoded());
 
 app.use(express.static(path.join(__dirname, "public"))); //76. Serving Files Statically
@@ -22,7 +21,9 @@ app.use(shop); // order of using the routes matter
 // it doesn't matter because we use get
 app.use((req, res, next) => {
   //res.status(404).send("<h1>page note found</h1>");
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+
+  //res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.render("404");
 });
 
 app.listen(3000);
