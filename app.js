750d712e8807af5f94ff578418e5e52f547f1a64
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-08-22 17:31:50 +0300

    92. Working with EJS

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+/*
+express-handlebars
 const expressHBS = require("express-handlebars");
 
 app.engine(
@@ -17,6 +19,10 @@ app.engine(
   })
 );
 app.set("view engine", "hbs");
+app.set("views", "views"); */
+
+// ejs
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const admin = require("./routes/admin"); // order of importing doesn't matter
@@ -35,7 +41,6 @@ app.use(shop); // order of using the routes matter
 // it doesn't matter because we use get
 app.use((req, res, next) => {
   //res.status(404).send("<h1>page note found</h1>");
-
   //res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
   res.status(404).render("404", { docTitle: "Page not found" });
 });
