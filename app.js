6273cd23224a0ebf4d1c5997e2cb0b78934036b1
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-09-21 16:16:43 +0300

    181. Installing the MongoDB Driver

--- app.js
+++ app.js
@@ -6,15 +6,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-// Data Base
-const sequelize = require("./util/database");
-const Product = require("./models/products");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/oder");
-const OrderItem = require("./models/order-item");
-
 // ejs
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -29,55 +20,18 @@ const productsController = require("./controllers/error");
 
 app.use(express.static(path.join(__dirname, "public"))); //76. Serving Files Statically
 
-
 app.use((req, res, next) => {
-  User.findByPk(1)
+  /*   User.findByPk(1)
     .then((user) => {
       req.user = user;
       next();
     })
     .catch((err) => {
       console.log(err);
-    });
+    }); */
 });
 
 app.use(admin);
 app.use(shop); // order of using the routes matter
 // it doesn't matter because we use get
 app.use(productsController.page404);
-
-Product.belongsTo(User, { constrains: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-
-User.hasOne(Cart);
-Cart.belongsTo(User);
-
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-//sync({ force: true })
-sequelize
-  //.sync({ force: true })
-  .sync()
-  .then((result) => {
-    //console.log(result);
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "ahmed", email: "ahmed@gmail.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    //console.log(typeof User);
-    user.createCart();
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
--- util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const db = new Sequelize("node-complete", "root", "ahmednodemysql", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports = db;
