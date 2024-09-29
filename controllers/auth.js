6c01209c7f96ecee66b69add453f3737daf75a22
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-09-29 15:16:33 +0300

    238. Configuring Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,15 @@ const Product = require("../models/product");
 const Order = require("../models/order");
 
 exports.getLogIn = (req, res, next) => {
+  const isLoggedIn = req.get("Cookie").split("=")[1];
+  console.log(isLoggedIn);
   res.render("auth/login", {
     pageTitle: "Loge In",
     path: "login",
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/");
 };
--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.get("/login", authController.getLogIn);
 
+router.post("/login", authController.postLogin);
+
 module.exports = router;
