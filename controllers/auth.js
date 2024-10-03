7eef173b261d1d84b924749e93016e1f1b17a18e
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-10-03 09:38:49 +0300

    293. Setup & Basic Validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 const sendEmail = require("../util/mailer").sendEmail;
+const { validationResult } = require("express-validator");
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -66,6 +67,16 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmedPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   if (!email || !password) {
     //console.log(!email || !password);
     return res.redirect("/signup");
